0f784f0c852e057b766f7d758a979bb9c1c2d676
shahin <mohammady.shahin@gmail.com>
2025-10-20 12:27:22 +0330

    update repo

--- test/api_custom_pages_test.go
+++ test/api_custom_pages_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
-	openapiclient "github.com/GIT_USER_ID/GIT_REPO_ID"
+	openapiclient "github.com/shaahinmo/cdn-go-sdk"
 )
 
 func Test_cdn-api_CustomPagesAPIService(t *testing.T) {
